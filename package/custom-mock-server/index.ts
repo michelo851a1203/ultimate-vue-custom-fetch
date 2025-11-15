755abe260d3cdb70cf6baccf9b3bdc603f52f800
MichaelHo <michelo851a1203@gmail.com>
2025-11-15 19:06:22 +0800

    feat(): add more testing # 2025-11-15-001
    	modified:   package/custom-mock-server/index.ts
    	modified:   package/test-custom-fetch/tests/auth.spec.ts

--- package/custom-mock-server/index.ts
+++ package/custom-mock-server/index.ts
@@ -2,9 +2,22 @@
 import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
 import { z } from 'zod';
-import { sign, verify, } from 'hono/jwt';
+import { sign, verify, jwt } from 'hono/jwt';
+import type { JwtVariables } from 'hono/jwt';
 
-const app = new Hono();
+type Variables = JwtVariables;
+
+export const userPayloadSchema = z.object({
+  sub: z.string(),
+  role: z.string(),
+  exp: z.number().int().nonnegative('請輸入正整數或0'),
+});
+
+export type UserPayload = z.infer<typeof userPayloadSchema>;
+
+const app = new Hono<{ Variables: Variables }>();
+
+const secret = '7a37ace2a64f2b5be4395cbf53b87e8c';
 
 export const postsSchema = z.object({
   name: z.string().min(1, '欄位必須'),
@@ -97,6 +110,78 @@ app.post('/postform', async (ctx) => {
   console.log(`\x1b[32m POST::/postForm postForm \x1b[0m name : ${name}, content : ${content}`);
   return ctx.json({
     message: `name is ${name}, content is ${content}`
+  }, 200)
+});
+
+app.post('/sign', async (ctx) => {
+  const original = {
+    sub: 'user1234',
+    role: 'admin',
+    exp: Math.floor(Date.now() / 1000) * 60 * 5,
+  }
+  const token = await sign(original, secret, 'HS256');
+  console.log(`\x1b[32m POST::/sign in to get token \x1b[0m token: \x1b[31m${token}\x1b[0m`);
+  return ctx.json({
+    token,
+    ...original,
+  }, 201)
+});
+
+app.use('/auth/*', jwt({ secret }));
+
+app.get('/auth/posts', async (ctx) => {
+  const payload = ctx.get('jwtPayload');
+  const name = ctx.req.query('name');
+  const content = ctx.req.query('content');
+  console.log(`\x1b[31m GET::/posts query string \x1b[0m query string -> name: ${name}, content: ${content}`);
+  return ctx.json({
+    name,
+    content,
+    ...payload,
+  });
+});
+
+app.post('auth/posts', zValidator('json', postsSchema), async (ctx) => {
+  const payload = ctx.get('jwtPayload');
+  const { name, content } = ctx.req.valid('json');
+  console.log(`\x1b[31m POST::/posts json input \x1b[0m json body -> name: ${name}, content: ${content}`);
+  return ctx.json({
+    message: `name : ${name} content: ${content ?? 'none'}`,
+    ...payload,
+  });
+});
+
+app.post('auth/upload', async (ctx) => {
+  const body = await ctx.req.parseBody()
+  const payload = ctx.get('jwtPayload');
+  const files = body['files'];
+  if (files instanceof File) {
+    console.log(`\x1b[31m POST::auth/upload FormData \x1b[0m file name : ${files.name}`);
+    return ctx.json({
+      name: files.name,
+      size: files.size,
+      type: files.type,
+      ...payload,
+    });
+  }
+  return ctx.json({ error: 'No files uploaded'}, 400)
+});
+
+app.post('auth/postform', async (ctx) => {
+  const body = await ctx.req.parseBody();
+  const payload = ctx.get('jwtPayload');
+  const name = body['name'];
+  const content = body['content'];
+
+  if (typeof name !== 'string' && typeof content !== 'string') {
+    return ctx.json({
+      error: 'not valid body'
+    }, 400)
+  }
+  console.log(`\x1b[31m POST::auth/postForm postForm \x1b[0m name : ${name}, content : ${content}`);
+  return ctx.json({
+    message: `name is ${name}, content is ${content}`,
+    ...payload,
   }, 200)
 });
 
--- package/test-custom-fetch/tests/auth.spec.ts
+++ package/test-custom-fetch/tests/auth.spec.ts
@@ -1,12 +1,116 @@
+import { z } from 'zod';
 import fs from 'fs';
 import { describe, it, expect } from 'vitest';
 import { useApi }  from '@demo-api-infra/custom-fetch';
 
-describe('testing with auth usage(JWT)', () => {
+describe('testing with auth usage(JWT)', async () => {
   const apiUtils = useApi();
 
-  it('', () => {
+  const postsSchema = z.object({
+    name: z.string().min(1, '欄位必須'),
+    content: z.string(),
+  });
 
-  })
+  const signResponseSchema = z.object({
+    token: z.string().min(1, 'required'),
+    sub: z.string().min(1, 'sub is required'),
+    role: z.string().min(1, 'role is required'),
+    exp: z.number().int().nonnegative('請輸入正整數或0'),
+  });
 
+  const { data: signInResponse, execute: signExecute } = apiUtils.post('/sign', {}, signResponseSchema);
+  await signExecute();
+  const signInValidator = signResponseSchema.safeParse(signInResponse.value);
+  expect(signInValidator.success).toBe(true);
+  expect(signInValidator.data?.token).toBeTypeOf('string');
+  const signResult = signInValidator.data;
+  const token = signResult?.token ?? '';
+
+  const original = !signResult ? null : {
+    sub: signResult.sub,
+    role: signResult.role,
+    exp: signResult.exp,
+  }
+  expect(original).not.toBe(null);
+  if (original === null) {
+    return;
+  }
+
+  it('get :: auth query string', async () => {
+    const query = {
+      name: 'testing',
+      content: 'hello',
+    }
+
+    const { data, execute } = apiUtils.getWithAuth(['/auth/posts', token], query, postsSchema);
+    await execute();
+    const expected = {
+      ...query,
+      ...original
+    }
+    expect(data.value).toMatchObject(expected);
+  });
+
+  it('post :: auth json post', async () => {
+    const jsonInput = {
+      name: 'testing cool',
+      content: 'this is content',
+    }
+    const expected = `name : ${jsonInput.name} content: ${jsonInput.content ?? 'none'}`;
+    const postsMessageJsonSchema = z.object({
+      message: z.string().min(1, 'required'),
+    });
+
+    const { data, execute } = apiUtils.postWithAuth(['auth/posts', token], jsonInput, postsMessageJsonSchema);
+    await execute();
+    expect(data.value).toMatchObject({
+      message: expected,
+      ...original,
+    });
+  });
+
+  it('post :: auth upload', async () => {
+    const uploadFileResponseSchema = z.object({
+      name: z.string().min(1, 'name required'),
+      size: z.number().positive(),
+      type: z.string().min(1, 'name required'),
+    });
+
+    const content = fs.readFileSync('./sample.txt')
+
+    const blob = new Blob([content], { type: 'text/plain' });
+    const form = new FormData()
+    form.append('files', blob, 'sample.txt');
+    expect(form.has('files')).toBe(true);
+    expect(form.get('files')).toBeInstanceOf(Blob);
+
+    const { data, execute } = apiUtils.uploadWithAuth(['auth/upload', token], form, uploadFileResponseSchema);
+    await execute();
+    expect(data.value).toMatchObject({
+      name: 'sample.txt',
+      size: blob.size,
+      type: 'text/plain;charset=utf-8',
+      ...original,
+    });
+  });
+
+  it('post :: auth postform', async () => {
+    const postFormResponseSchema = z.object({
+      message: z.string(),
+    });
+
+    const postForm = new URLSearchParams();
+    postForm.append('name', 'sample content');
+    postForm.append('content', 'sample content');
+    const postFormJsonInput = {
+      name: 'sample content',
+      content: 'sample content',
+    }
+    const { data, execute } = apiUtils.postFormWithAuth(['auth/postform', token], postFormJsonInput, postFormResponseSchema);
+    await execute();
+    expect(data.value).toMatchObject({
+      message: `name is ${postFormJsonInput.name}, content is ${postFormJsonInput.content}`,
+      ...original
+    });
+  });
 });
